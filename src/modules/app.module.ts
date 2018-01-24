1c05a1fbc4d811d2a10cd619e93cef3023222189
jestersimpps <jestersimpps@gmail.com>
2018-01-24 16:32:41 +0100

    hlfinit in app module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,12 +2,14 @@ import { EventsModule } from './events.module';
 import { ChainModule } from './chain.module';
 import { QueueModule } from './queue.module';
 import { EnvConfig } from './../config/env';
-import { Log } from 'hlf-node-utils';
+import { Log, FabricOptions } from 'hlf-node-utils';
 import { Module } from '@nestjs/common';
 import { PingService } from '../routes/ping/ping.service';
 import { PingController } from '../routes/ping/ping.controller';
 import { AssetsController } from '../routes/assets/assets.controller';
 import { AssetsService } from '../routes/assets/assets.service';
+import { HlfClient } from '../services/chain/hlfclient';
+import { QueueListenerService } from '../services/queue/queuelistener.service';
 
 @Module({
     controllers: [
@@ -25,9 +27,33 @@ import { AssetsService } from '../routes/assets/assets.service';
     ],
 })
 export class ApplicationModule {
-    constructor() {
-        // list env keys in cli
-        for (let propName of Object.keys(EnvConfig)) {
+
+    /**
+     * Creates an instance of PingService.
+     * Will init the hlf client
+     * @param {HlfClient} hlfClient 
+     * @memberof PingService
+     */
+    constructor(
+        private hlfClient: HlfClient,
+        private queueListenerService: QueueListenerService) {
+
+        this.hlfClient.setOptions(<FabricOptions>{
+            walletPath: `./src/config/creds`,
+            userId: 'PeerAdmin',
+            channelId: 'mychannel',
+            networkUrl: `grpc://${EnvConfig.PEER_HOST}:7051`,
+            eventUrl: `grpc://${EnvConfig.PEER_HOST}:7053`,
+            ordererUrl: `grpc://${EnvConfig.ORDERER_HOST}:7050`
+        });
+
+        this.hlfClient.init().then(result => {
+            Log.awssqs.info(`Starting Queue Listener...`);
+            this.queueListenerService.init();
+        });
+
+         // list env keys in cli
+         for (let propName of Object.keys(EnvConfig)) {
             Log.config.debug(`${propName}:  ${EnvConfig[propName]}`);
         }
     }
--- src/routes/ping/ping.service.ts
+++ src/routes/ping/ping.service.ts
@@ -1,38 +1,14 @@
-import { FabricOptions } from 'hlf-node-utils/dist/models/fabricoptions.model';
 import { Component } from '@nestjs/common';
-import { Log } from 'hlf-node-utils';
-import { HlfClient } from '../../services/chain/hlfclient';
-import { QueueListenerService } from '../../services/queue/queuelistener.service';
-import { EnvConfig } from '../../config/env';
 
 @Component()
 export class PingService {
 
     /**
-     * Creates an instance of PingService.
-     * Will init the hlf client
-     * @param {HlfClient} hlfClient 
+     * check if app is alive
+     * 
+     * @returns {string} 
      * @memberof PingService
      */
-    constructor(
-        private hlfClient: HlfClient,
-        private queueListenerService: QueueListenerService) {
-
-        this.hlfClient.setOptions(<FabricOptions>{
-            walletPath: `./src/config/creds`,
-            userId: 'PeerAdmin',
-            channelId: 'mychannel',
-            networkUrl: `grpc://${EnvConfig.PEER_HOST}:7051`,
-            eventUrl: `grpc://${EnvConfig.PEER_HOST}:7053`,
-            ordererUrl: `grpc://${EnvConfig.ORDERER_HOST}:7050`
-        });
-
-        this.hlfClient.init().then(result => {
-            Log.awssqs.info(`Starting Queue Listener...`);
-            this.queueListenerService.init();
-        });
-    }
-
     ping(): string {
         return 'Chain-service is alive!';
     }
