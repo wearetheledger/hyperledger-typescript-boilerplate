7f475edfd177816b6e21cc1d982e63eb9720f422
jestersimpps <jestersimpps@gmail.com>
2018-03-01 13:40:55 +0100

    fetch user metadate wip

--- src/services/authentication/auth0/auth0.service.ts
+++ src/services/authentication/auth0/auth0.service.ts
@@ -1,4 +1,4 @@
-
+import { EnvConfig } from './../../../config/env';
 import { Component, InternalServerErrorException } from '@nestjs/common';
 import * as jwtDecode from 'jwt-decode';
 import { JwtToken } from 'auth0';
@@ -9,56 +9,55 @@ import { Auth0UserModel } from './auth0user.model';
 @Component()
 export class Auth0Service {
 
-    constructor(private hlfCaClient: HlfCaClient) {
-
-    }
-
-    enrollUser(auth0UserModel: Auth0UserModel): void {
-        this.hlfCaClient.createUser(
-            auth0UserModel.id,
-            'Org1MSP',
-            'org1.department1',
-            this.transformAttrs(auth0UserModel)
-        ).then(user => {
-            console.log(user);
-        });
-    }
+    constructor(private hlfCaClient: HlfCaClient) { }
 
     getUserId(auth): Promise<string> {
         let auth0UserModel;
-        if (auth) {
-            let token: JwtToken = jwtDecode(auth.split(' ')[1]);
-            const userId = token.replace('|', '-');
-            // TODO: call Auth0api to fetch metadata
-            auth0UserModel = {
-                id: userId,
-                username: userId,
-                email: null,
-                user_metadata: {}
-            };
-        } else {
-            auth0UserModel = {
-                id: 'guest',
-                username: 'guest',
-                email: null,
-                user_metadata: {
-                    role: 'guest'
+        return Promise.resolve()
+            .then((): Promise<Auth0UserModel> => {
+                if (auth) {
+                    let token: JwtToken = jwtDecode(auth.split(' ')[1]);
+                    const userId = token.sub.replace('|', '-');
+                    // TODO: call Auth0api to fetch metadata
+                    // https://tld-greencard.eu.auth0.com/api/v2/users/id
+                    return Promise.resolve({
+                        id: userId,
+                        username:userId,
+                        email: null,
+                        user_metadata: {}
+                    })
+                } else {
+                    return Promise.resolve({
+                        id: 'guest',
+                        username: 'guest',
+                        email: null,
+                        user_metadata: {
+                            role: 'guest'
+                        }
+                    })
                 }
-            };
-        }
-        return this.hlfCaClient.getUserFromStore(auth0UserModel.id)
+            })
+            .then((resolvedAuth0UserModel: Auth0UserModel) => {
+                auth0UserModel = resolvedAuth0UserModel;
+                return this.hlfCaClient.getUserFromStore(resolvedAuth0UserModel.id)
+            })
             .then(storeUser => {
                 if (storeUser) {
                     return Promise.resolve(auth0UserModel.id);
                 } else {
-                    return this.hlfCaClient.createUser(auth0UserModel.id, 'Org1MSP', 'org1.department1', this.transformAttrs(auth0UserModel));
+                    return this.hlfCaClient.createUser(
+                        auth0UserModel.id,
+                        'Org1MSP',
+                        'org1.department1',
+                        this.transformAttrs(auth0UserModel)
+                    );
                 }
             })
             .then(newUser => {
                 return Promise.resolve(auth0UserModel.id);
             })
             .catch(error => {
-                throw new InternalServerErrorException('Unable to create user certificates for: ' + auth0UserModel.id);
+                throw new InternalServerErrorException('Failed to create user certificates for: ' + auth0UserModel.id);
             });
     }
 
