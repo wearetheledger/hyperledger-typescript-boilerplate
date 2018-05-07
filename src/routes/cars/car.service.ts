f0290af516bdd264e20ff0650497dc1acd5c62ae
jestersimpps <jestersimpps@gmail.com>
2018-05-07 15:44:46 +0200

    object to array converter

--- src/routes/cars/car.service.ts
+++ src/routes/cars/car.service.ts
@@ -7,64 +7,68 @@ import { IAuthUser } from '../../services/authentication/authenticateduser';
 
 @Component()
 export class CarService {
+  /**
+   * Creates an instance of CarService.
+   * @param {RequestHelper} requestHelper
+   * @memberof CarService
+   */
+  constructor(private requestHelper: RequestHelper) {}
 
-    /**
-     * Creates an instance of CarService.
-     * @param {RequestHelper} requestHelper
-     * @memberof CarService
-     */
-    constructor(private requestHelper: RequestHelper) {
-    }
+  /**
+   * Get all cars
+   *
+   * @returns {Promise<CarDto[]>}
+   * @memberof CarService
+   */
+  getAll(): Promise<CarDto[]> {
+    return this.requestHelper.queryRequest(ChainMethod.queryAllCars, {}).catch((error) => {
+      throw new InternalServerErrorException(error);
+    });
+  }
 
-    /**
-     * Get all cars
-     *
-     * @returns {Promise<CarDto[]>}
-     * @memberof CarService
-     */
-    getAll(): Promise<CarDto[]> {
-        return this.requestHelper.queryRequest(ChainMethod.queryAllCars, [])
-            .catch(error => {
-                throw new InternalServerErrorException(error);
-            });
-    }
+  /**
+   * Get car by id
+   *
+   * @returns {Promise<CarDto>}
+   * @memberof CarService
+   */
+  getById(id: string): Promise<CarDto> {
+    return this.requestHelper.queryRequest(ChainMethod.queryCar, { id }).then(
+      (car) => {
+        if (!car) {
+          throw new NotFoundException('Car does not exist!');
+        }
+        return car;
+      },
+      (error) => {
+        throw new InternalServerErrorException(error);
+      },
+    );
+  }
 
-    /**
-     * Get car by id
-     *
-     * @returns {Promise<CarDto>}
-     * @memberof CarService
-     */
-    getById(id: string): Promise<CarDto> {
-        return this.requestHelper.queryRequest(ChainMethod.queryCar, [id])
-            .then(car => {
-                if (!car) {
-                    throw new NotFoundException('Car does not exist!');
-                }
-                return car;
-            }, error => {
-                throw new InternalServerErrorException(error);
-            });
-    }
-
-    /**
-     * Create new car
-     *
-     * @param {CarDto} carDto
-     * @param {IAuthUser} authUser
-     * @returns {Promise<InvokeResult>}
-     * @memberof CarService
-     */
-    create(carDto: CarDto, authUser: IAuthUser): Promise<InvokeResult> {
-        return this.requestHelper.invokeRequest(ChainMethod.createCar, [
-            carDto.colour,
-            carDto.key,
-            carDto.make,
-            carDto.model,
-            carDto.owner,
-        ], authUser.id)
-            .catch(error => {
-                throw new InternalServerErrorException(error);
-            });
-    }
+  /**
+   * Create new car
+   *
+   * @param {CarDto} carDto
+   * @param {IAuthUser} authUser
+   * @returns {Promise<InvokeResult>}
+   * @memberof CarService
+   */
+  create(carDto: CarDto, authUser: IAuthUser): Promise<InvokeResult> {
+    return this.requestHelper
+      .invokeRequest(
+        ChainMethod.createCar,
+        {
+          colour: carDto.colour,
+          key: carDto.key,
+          make: carDto.make,
+          model: carDto.model,
+          owner: carDto.owner,
+        },
+        authUser.id,
+      )
+      .catch((error) => {
+        throw new InternalServerErrorException(error);
+      });
+  }
 }
--- src/services/chain/requesthelper.ts
+++ src/services/chain/requesthelper.ts
@@ -11,110 +11,98 @@ import { IEventService } from '../events/event.interface';
 
 @Component()
 export class RequestHelper {
+  // TODO: refactor invokes according to https://docs.nestjs.com/recipes/cqrs
 
-    // TODO: refactor invokes according to https://docs.nestjs.com/recipes/cqrs
+  /**
+   * Creates an instance of RequestHelper.
+   * @param {HlfClient} hlfClient
+   * @param {PusherService} eventService
+   * @param {QueuePusherService} queuePusherService
+   * @memberof RequestHelper
+   */
+  constructor(
+    private hlfClient: HlfClient,
+    @Inject('IEventService') private eventService: IEventService,
+    private queuePusherService: QueuePusherService,
+  ) {}
 
-    /**
-     * Creates an instance of RequestHelper.
-     * @param {HlfClient} hlfClient
-     * @param {PusherService} eventService
-     * @param {QueuePusherService} queuePusherService
-     * @memberof RequestHelper
-     */
-    constructor(private hlfClient: HlfClient,
-                @Inject('IEventService') private eventService: IEventService,
-                private queuePusherService: QueuePusherService) {
-    }
+  /**
+   * Pass transaction request to aws queue
+   *
+   * @param {ChainMethod} chainMethod
+   * @param {Object} params
+   * @param {string} userId
+   * @param invokeAlways - Workaround for message deduplication SQS
+   * @returns {Promise<InvokeResult>}
+   * @memberof RequestHelper
+   */
+  public invokeRequest(chainMethod: ChainMethod, params: Object, userId: string, invokeAlways = false): Promise<InvokeResult | any> {
+    const arrayParams = this.convertObjectParams(params);
 
-    /**
-     * Pass transaction request to aws queue
-     *
-     * @param {ChainMethod} chainMethod
-     * @param {string[]} params
-     * @param {string} userId
-     * @param invokeAlways - Workaround for message deduplication SQS
-     * @returns {Promise<InvokeResult>}
-     * @memberof RequestHelper
-     */
-    public invokeRequest(chainMethod: ChainMethod, params: string[], userId: string, invokeAlways = false): Promise<InvokeResult | any> {
-
-        params = this.forceStringParams(params);
-
-        if (EnvConfig.BYPASS_QUEUE) {
-            return this.hlfClient.invoke(chainMethod, params)
-                .then((response) => {
-                    Log.hlf.debug('Invoke successfully executed: ', response);
-                    this.eventService.triggerSuccess(userId, chainMethod, params);
-                    return {txHash: response};
-                })
-                .catch(error => {
-                    Log.hlf.error(`${chainMethod}`, error);
-                    this.eventService.triggerError(userId, chainMethod, params);
-                    throw error;
-                });
-        } else {
-            return this.queuePusherService.add(chainMethod, params, userId, invokeAlways)
-                .then((response) => {
-                    Log.awssqs.debug('Invoke successfully added to SQS queue: ', response);
-                    return response;
-                })
-                .catch(error => {
-                    Log.awssqs.error(`${chainMethod}`, error);
-                    throw error;
-                });
-        }
-    }
-
-    /**
-     * Query hlf chain and return response
-     *
-     * @param {ChainMethod} chainMethod
-     * @param {string[]} params
-     * @returns {Promise<any>}
-     * @memberof RequestHelper
-     */
-    public queryRequest(chainMethod: ChainMethod, params: string[]): Promise<any> {
-        params = this.forceStringParams(params);
-
-        return this.hlfClient.query(chainMethod, params)
-            .then((response) => {
-                Log.hlf.debug('Query successfully executed!');
-                return response;
-            })
-            .catch(error => {
-                Log.hlf.error(`${chainMethod}`, error);
-                throw error;
-            });
-    }
-
-    /**
-     * validate requests with yup
-     *
-     * @param {Schema} schema
-     * @param {any} body
-     * @returns {Promise<any>}
-     * @memberof RequestHelper
-     */
-    public validateRequest(schema: Schema, body): Promise<any[]> {
-        return schema.validate(body)
-            .then(params => {
-                Log.config.debug('Valid object schema: ', params);
-                return params;
-            })
-            .catch((error) => {
-                Log.config.error('Validation', error);
-                throw error;
-            });
-    }
-
-    private forceStringParams(params: string[]): string[] {
-        return params.map(param => {
-            if (isObject(param)) {
-                return JSON.stringify(param);
-            }
-
-            return param.toString();
+    if (EnvConfig.BYPASS_QUEUE) {
+      return this.hlfClient
+        .invoke(chainMethod, arrayParams)
+        .then((response) => {
+          Log.hlf.debug('Invoke successfully executed: ', response);
+          this.eventService.triggerSuccess(userId, chainMethod, params);
+          return { txHash: response };
+        })
+        .catch((error) => {
+          Log.hlf.error(`${chainMethod}`, error);
+          this.eventService.triggerError(userId, chainMethod, params);
+          throw error;
+        });
+    } else {
+      return this.queuePusherService
+        .add(chainMethod, arrayParams, userId, invokeAlways)
+        .then((response) => {
+          Log.awssqs.debug('Invoke successfully added to SQS queue: ', response);
+          return response;
+        })
+        .catch((error) => {
+          Log.awssqs.error(`${chainMethod}`, error);
+          throw error;
         });
     }
+  }
 
+  /**
+   * Query hlf chain and return response
+   *
+   * @param {ChainMethod} chainMethod
+   * @param {Object} params
+   * @returns {Promise<any>}
+   * @memberof RequestHelper
+   */
+  public queryRequest(chainMethod: ChainMethod, params: Object): Promise<any> {
+    const arrayParams = this.convertObjectParams(params);
+
+    return this.hlfClient
+      .query(chainMethod, arrayParams)
+      .then((response) => {
+        Log.hlf.debug('Query successfully executed!');
+        return response;
+      })
+      .catch((error) => {
+        Log.hlf.error(`${chainMethod}`, error);
+        throw error;
+      });
+  }
+
+  /**
+   * Convert object to string array
+   *
+   * @private
+   * @param {Object} paramsS
+   * @returns {string[]}
+   * @memberof RequestHelper
+   */
+  private convertObjectParams(params: Object): string[] {
+    return Object.keys(params).map((key) => {
+      if (isObject(params[key])) {
+        return JSON.stringify(params[key]);
+      }
+      return params[key].toString();
+    });
+  }
 }
